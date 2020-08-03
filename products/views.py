6a27806afb090153700b2456c357f41b81108efd
Hari <baluoger91@gmail.com>
2020-08-03 18:22:37 +0530

    Products is working

--- products/views.py
+++ products/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def index(request):
+    return HttpResponse("<h1>Index Page</h1>")
+
+def about(request):
+    return HttpResponse("<h1>About Page</h1>")
